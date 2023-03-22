f274c8d7bed17abf6ad19347b676537682daf85e
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-03-21 22:54:16 -0300

    primeiros passos com swing

---
+++ src/Conversor_De_Moedas/Main.java
@@ -0,0 +1,12 @@
+package Conversor_De_Moedas;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String opcoes = JOptionPane.showInputDialog(null, "Bem vindo", "Menu", JOptionPane.QUESTION_MESSAGE);
+
+	}
+
+}
